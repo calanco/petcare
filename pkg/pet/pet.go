f5ae4bef7505b3fd57d9b05337cba8b482e696e5
Raffaello Perez De Vera <r.perezdevera@gmail.com>
2020-10-10 22:20:10 +0200

    Allowed years to be passed as 0

--- pkg/pet/pet.go
+++ pkg/pet/pet.go
@@ -5,7 +5,7 @@ type Pet struct {
 	Name   string `json:"name"`
 	Breed  Breed  `json:"breed,omitempty"`
 	Size   Size   `json:"size,omitempty"`
-	Years  Years  `json:"years,omitempty"`
+	Years  *Years `json:"years,omitempty"`
 	Months Months `json:"months,omitempty"`
 	Weight Weight `json:"weight,omitempty"`
 }
